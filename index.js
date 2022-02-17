b1eb9bf6cd6e23eb3472b4a6ee7d982d7c39450c
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-02-17 14:23:04 -0800

    completed MVP Tasks 1-4.

--- index.js
+++ index.js
@@ -42,7 +42,30 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+
+  }
+  eat(food){
+
+    if(this.stomach.length < 10){
+      this.stomach.push(food);
+      return 'food was yummy!';
+    }else {
+      return `the stomach is full`;
+    }
+}
+
+poop(){
+  this.stomach = [];
+  return `stomach is empty`;
+}
+
+toString(){
+  return `${this.name}, ${this.age};`
+}
 }
 
 /*
@@ -60,7 +83,33 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;  
+  } 
+
+fill (gallon){
+  this.tank = this.tank + gallon;
+}
+drive (distance){
+
+  const driveableMiles = this.tank * this.milesPerGallon;
+
+  if ( distance <= driveableMiles  ){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank -(distance/this.milesPerGallon);
+
+
+  }else{
+    this.odometer = this.odometer +driveableMiles;
+    this.tank = 0;
+    return `I ran out of the fuel at ${this.odometer} miles!`;
+
+
+  }
+}
 }
 
 /*
@@ -76,6 +125,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(props){
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
   
 }
 
@@ -93,7 +150,23 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(props){
+    super(props);
+    this.specialty = props.specialty;
+    this.favLanguage = props.favLanguage;
+    this.catchPhrase = props.catchPhrase;
+    this.subject = props.subject;
+    this.student = props.student;
+  }
+  demo(){
+    return `Today we are learning about ${this.subject}`;
+
+    }
+
+  grade(){
+    return `${this.student.name} receives a perfect score on ${this.subject}`;
+  }
 
 }
 /*
@@ -111,7 +184,12 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian{
+  constructor(props){
+    super(props);
+
+
+  }
    
 }
 
